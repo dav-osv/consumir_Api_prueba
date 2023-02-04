8ad1e962da89f8795bc3ac1aba3d2fce9fd0497c
David <david.rodriguez.7@outlook.com>
2023-02-03 18:59:26 -0600

    add prueba -proyect reloj 2

---
+++ ApiRest.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ProyectoReloj
+{
+
+
+    public class ApiRest
+    {
+
+        private const string username = "admin";
+        private const string password = "reloj4891";
+        private const string endpoint = "http://192.168.100.6";
+
+        public async Task<JObject> obtenerRegistrosEmpleados()
+        {
+            try
+            {
+                Uri uri = new Uri(endpoint+"/ISAPI/AccessControl/AcsEvent?format=json");
+                Console.WriteLine("url: " + uri.ToString());
+                var credentialCache = new CredentialCache();
+                credentialCache.Add(uri, "Digest", new NetworkCredential(username, password));
+
+                var handler = new HttpClientHandler()
+                {
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                    Credentials = credentialCache
+                };
+
+                using (var HttClient = new HttpClient())
+                {
+                    var httpClient = new HttpClient(handler);
+                    var myObject = new
+                    {
+                        searchID = "0",
+                        searchResultPosition = 0,
+                        maxResults = 1000,
+                        major = 5,
+                        minor = 0,
+                        startTime = "2023-01-01T00:00:00-00:00",
+                        endTime = "2023-01-07T00:00:00-00:00"
+                    };
+
+                    var myEvent = new
+                    {
+                        AcsEventCond = myObject
+                    };
+
+                    var httpContent = new StringContent(JsonConvert.SerializeObject(myEvent), Encoding.UTF8, "application/json");
+                    var httpResponse = await httpClient.PostAsync(uri, httpContent);
+
+                    if (httpResponse.IsSuccessStatusCode) {
+                        var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                        return JObject.Parse(responseContent);
+
+                    }
+
+                    Console.WriteLine("Hubo un error en la peticion codigo [" + httpResponse.IsSuccessStatusCode + "]" );
+                    return JObject.Parse("{success: False, mensaje: Hubo un error en la peticion codigo [\" + httpResponse.IsSuccessStatusCode + \"]\"  }");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+                return JObject.Parse("{success:False, mensaje:"+ e.Message+"}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Net;
+using System;
+using System.Runtime.InteropServices;
+using ProyectoReloj;
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+
+public class Program
+{
+   
+    public static void Main(string[] args)
+    {
+              ApiRest apiRest = new ApiRest();
+         
+              var registrosEmpleados = apiRest.obtenerRegistrosEmpleados().Result;
+
+
+                /*  El proyecto se tiene que compilar en NET. 6 u 7 */
+                /*  En el paquete nugets se tiene que instalar la siguiente libreria  Newtonsoft.Json.Linq; para que pueda funcionar */
+
+
+                /* Una vez teniendo la variable registroEmpleados, simplemente verificamos que venga algo , en caso contrario es un error*/
+                /* si viene algo siemplemente recorremos los atributos con un foreach y los obtenemos las propiedades de los items */
+
+                /* if (registrosEmpleados != null )
+                    foreach (var item in registrosEmpleados["AcsEvent"]["InfoList"]){
+                      Console.WriteLine(item["time"]);
+                } */
+
+
+
+    }
+}
